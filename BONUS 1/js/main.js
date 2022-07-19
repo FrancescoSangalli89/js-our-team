4957498fcf883520bb7c02bbfa8cb68219e86621
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-07-19 09:33:52 +0200

    style: modified card style

--- BONUS 1/js/main.js
+++ BONUS 1/js/main.js
@@ -1,23 +1,3 @@
-// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team./
-// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto./
-// MILESTONE 0:
-// Creare l’array di oggetti con le informazioni fornite./
-// MILESTONE 1:
-// Stampare su console le informazioni di nome, ruolo e la stringa della foto/
-// MILESTONE 2:
-// Stampare le stesse informazioni su DOM sottoforma di stringhe/
-// BONUS 1:
-// Trasformare la stringa foto in una immagine effettiva
-// BONUS 2:
-// Organizzare i singoli membri in card/schede
-
-// NOME                  RUOLO                        IMMAGINE
-// Wayne Barnett	      Founder & CEO	               wayne - barnett - founder - ceo.jpg
-// Angela Caroll	      Chief Editor	               angela - caroll - chief - editor.jpg
-// Walter Gordon	      Office Manager	           walter - gordon - office - manager.jpg
-// Angela Lopez	      Social Media Manager	       angela - lopez - social - media - manager.jpg
-// Scott Estrada	      Developer	                   scott - estrada - developer.jpg
-// Barbara Ramos	      Graphic Designer	           barbara - ramos - graphic - designer.jpg
 
 const team = [
 
@@ -65,8 +45,10 @@ for (let i = 0; i < team.length; i++) {
 
 
     const card = document.createElement('div');
-    card.style.width = `calc(100% / 3)`;
+    card.style.width = `calc(100% / 3 - 20px)`;
     card.style.padding = `10px`;
+    card.style.margin = '10px';
+    card.style.border = '1px solid grey';
 
     const imageCard = document.createElement('div');
 
@@ -82,6 +64,8 @@ for (let i = 0; i < team.length; i++) {
 
     const name = document.createElement('div');
     name.append(team[i].nome);
+    name.style.fontWeight = 800;
+    name.style.marginBottom = '10px';
     text.append(name);
 
     const role = document.createElement('div');
